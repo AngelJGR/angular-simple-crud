39da1e0e04597c0e2e80cf5aa521bb357d7e0970
AngelJGR <ajguerreror@gmail.com>
2020-05-30 12:03:00 -0400

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
 
+import { Employee } from './Models/employee';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-practica-uno';
+
+  employees: Array<Employee> = [
+    { id: 1, name: "Ryan", country: "USA" },
+    { id: 2, name: "Jhon", country: "Canada" },
+    { id: 3, name: "Zac", country: "USA" }
+  ];
+
+  selectedEmployee: Employee = new Employee();
+
+  constructor () { }
+
+  addOrEdit(): void{
+    if (this.selectedEmployee.id === undefined){
+      this.selectedEmployee.id = this.employees.length + 1;
+      this.employees.push(this.selectedEmployee);
+    }
+    this.selectedEmployee = new Employee();
+  }
+
+  openForEdit(employee: Employee): void{
+    this.selectedEmployee = employee;
+  }
+
+  cancel():void{
+    this.selectedEmployee = new Employee();
+  }
+
+  delete():void{
+    this.employees = this.employees.filter(el => el.id != this.selectedEmployee.id);
+    this.selectedEmployee = new Employee();
+  }
+
 }
